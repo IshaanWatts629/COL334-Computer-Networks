e97902258a98cf611b63117930b9bd326b6a23f4
Ishaan Watts <84903995+IshaanWatts629@users.noreply.github.com>
2022-11-30 01:13:14 +0530

    Delete Assignment 2 directory

--- Assignment 2/2019PH10629.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python 2019PH10629_server.py &
-python 2019PH10629_client.py
--- Assignment 2/2019PH10629_2.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python 2019PH10629_server2.py &
-python 2019PH10629_client2.py
